6c1cc6b1646b2b04f7b602e50424acf022ae0146
Harrison DeFord <hdeford@baltimorecountymd.gov>
2025-08-28 12:24:35 -0400

    added scrabble frequencies

--- src/wordle_difficulty_by_letter_frequency.R
+++ src/wordle_difficulty_by_letter_frequency.R
@@ -32,6 +32,17 @@ wordle_df <- read.table(text = final_string,
   mutate(Date = as_date(Date, format = "%m/%d/%y"),
          grp = ceiling(row_number()/12))
 
+prop_from_scrabble <- words::words |> 
+  filter(word_length == 5) |>
+  paste(collapse = "") |>
+  str_split(pattern = "") |>
+  table() |>
+  as.data.frame() |>
+  filter(toupper(Var1) %in% LETTERS) |>
+  mutate(scrabble_prop = Freq / sum(Freq),
+         Var1 = toupper(Var1)) |>
+  rename(Letter = Var1)
+
 letter_frequencies <- wordle_df$Word |>
   paste(collapse = "") |>
   str_split(pattern = "") |>
@@ -40,22 +51,31 @@ letter_frequencies <- wordle_df$Word |>
   mutate(prop = Freq / sum(Freq)) |>
   rename(Letter = Var1)
 
+difficulties <- letter_frequencies |>
+  left_join(prop_from_scrabble, by = "Letter") |>
+  select(-starts_with("Freq"))
+
 wordle_df_letters <- wordle_df |>
   separate_longer_position(cols = Word,
                            width = 1) |>
-  left_join(letter_frequencies, by = join_by("Word" == "Letter")) |>
+  left_join(difficulties, by = join_by("Word" == "Letter")) |>
   group_by(Number) |>
-  summarize(avg_prop = mean(prop))
+  summarize(avg_prop = mean(prop),
+            avg_scrabble_prop = mean(scrabble_prop))
 
 wordle_df_difficulty <- wordle_df |>
-  left_join(wordle_df_letters, by = "Number")
+  left_join(wordle_df_letters, by = "Number") |>
+  pivot_longer(cols = c("avg_prop", "avg_scrabble_prop")) |>
+  mutate(`Letter Source` = case_when(name == 'avg_prop' ~ "Wordle",
+                          TRUE ~ "Scrabble"))
 
 ggplot(wordle_df_difficulty) +
-  geom_point(aes(x = Date, y = avg_prop)) +
-  geom_smooth(aes(x = Date, y = avg_prop), method = "loess") +
+  geom_point(data = filter(wordle_df_difficulty, name == "avg_prop"), aes(x = Date, y = value), color = "grey20", alpha = 0.3) +
+  geom_smooth(aes(x = Date, y = value, group = `Letter Source`, color = `Letter Source`), method = "loess") +
   labs(title = "Difficulty of Wordle words over time",
        subtitle = "As defined by letter frequency",
        y = "Average Letter Proportion") +
-  geom_label_repel(data = filter(wordle_df_difficulty, avg_prop > 0.082 | avg_prop < 0.03),
-                   aes(x = Date, y = avg_prop, label = Word)) +
+  geom_label_repel(data = filter(wordle_df_difficulty, name == "avg_prop" & (value > 0.082 | value < 0.03)),
+                   aes(x = Date, y = value, label = Word)) +
   theme_minimal()
+
